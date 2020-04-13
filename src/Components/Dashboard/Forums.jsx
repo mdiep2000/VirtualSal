fae25c6a366743e3068d257eb83f6ec497272770
Mindy Diep <mindydiep@Mindys-MacBook-Pro.local>
2020-04-12 17:58:20 -0700

    updated forums

--- src/Components/Dashboard/Forums.jsx
+++ src/Components/Dashboard/Forums.jsx
@@ -3,7 +3,15 @@ import React, { Component } from "react";
 class Forums extends Component {
   state = {};
   render() {
-    return <h1>forums</h1>;
+    return (
+      <div>
+        <h2>Forum #1</h2>
+        <h2>Forum #2</h2>
+        <h2>Forum #3</h2>
+        <h2>Forum #4</h2>
+        <h2>Forum #5</h2>
+      </div>
+    );
   }
 }
 
