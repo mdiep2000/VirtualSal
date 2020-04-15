cc2a9dcbdf193fac900cefb5c58b5df4a5e7889f
joshmeidl <josh_meidl@Joshs-MacBook-Pro.local>
2020-04-15 13:12:33 -0700

    several changes, beginning to get search results to link

--- src/Components/Dashboard/Info.jsx
+++ src/Components/Dashboard/Info.jsx
@@ -1,88 +1,9 @@
 import React, { Component } from "react";
-import "./Info.css"
 
 class Info extends Component {
-  state = {
-    course: "",
-    professor: "",
-    workload: 0,
-    clarity: 0,
-    comment: ""
-
-  };
-  change = e => {
-    this.setState({
-      [e.target.name] : e.target.value
-    });
-  }
-  handleSubmit = e => {
-    console.log(this.state);
-    if(!(this.state.course && this.state.professor && this.state.workload && this.state.clarity && this.state.comment)){
-      alert("One or more fields left blank");
-      e.preventDefault();
-      e.stopPropogation();
-    }
-    //log into SQL database
-    return;
-  }
+  state = {};
   render() {
-    return (
-    <div>
-      <h1 class = "header">Create a Review</h1>
-      <br />
-      <form onSubmit ={this.handleSubmit}>
-      <div className = "course">
-      <select name="course" value={this.state.course} onChange = {e => this.change(e)}>
-        <option value="none" selected disabled hidden>Course</option>
-        <option value="CSCI 103">CSCI 103</option>
-        <option value="CSCI 109">CSCI 109</option>
-        <option value="CSCI 170">CSCI 170</option>
-        <option value="CSCI 201">CSCI 201</option>
-        <option value="CSCI 270">CSCI 270</option>
-        <option value="CSCI 310">CSCI 310</option>
-      </select>
-      </div>
-      <br />
-      <div className="professor">
-      <input name="professor" placeholder="professor" value={this.state.professor} onChange = {e=>this.change(e)}/>
-      </div>
-
-    <div className="workload">
-      <select name="workload" value={this.state.workload}>
-      <option value="none" selected disabled hidden>Workload</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <p className = "clarification">1: Light, 5: Extreme</p>
-    </div>
-
-    <div className="clarity">
-      <select name="clarity" value={this.state.clarity} onChange = {e=>this.change(e)}>
-      <option value="none" selected disabled hidden>Clarity</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <p className = "clarification">1: Muddy, 5: Clear</p>
-    </div>
-
-      <div className="form-group w-50">
-      <textarea type = "text" name="comment" value ={this.state.comment} placeholder="comment" class = "form-control" onChange={e=>this.change(e)} />
-      </div>
-      <br />
-      <div className="submit">
-      <input type="submit" value="Submit" />
-      </div>
-      </form>
-      
-
-    </div>
-    )
+    return <h1>Info</h1>;
   }
 }
 
--- src/Components/Dashboard/SearchResults.jsx
+++ src/Components/Dashboard/SearchResults.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Forums from "./Forums";
+import Course from "./Course";
+import Review from "./Review";
 /*Ahmed, I'm gonna pass the search key in as a prop, so once this page renders we can query backend which will return list of results.
 You can display these however you want, but I assume theyll have name and link to page*/
 
@@ -23,10 +26,34 @@ var exampleList = [
   {
     type: "review",
     id: 2,
+    data: {
+      course: "CSCI-104 Data Structures and Object Oriented Programming",
+      professor: "cote",
+      workload: 3,
+      clarity: 4,
+      comment: "good prof",
+    },
   },
   {
     type: "forum",
     id: 3,
+    data: {
+      question: "other exanoke?",
+      upvotes: 5,
+      downvotes: 4,
+      thread: [
+        {
+          comment: "example ",
+          upvotes: 1,
+          downvotes: 3,
+        },
+        {
+          comment: "example ",
+          upvotes: 3,
+          downvotes: 1,
+        },
+      ],
+    },
   },
 ];
 class SearchResults extends Component {
@@ -53,28 +80,66 @@ class SearchResults extends Component {
   };
 
   render() {
-    //   var searchLinks = this.state.resultsList.map((resultID) => (
-    //     <Link key={resultID} to={"dashboard/result=" + resultID.toString()}>
-    //       {"link" + resultID.toString()}
-    //     </Link>
-    //    ));
-    //    for(var i = 0; i<this.state.resultsList.length; i++){
-    //       var component = null;
-    //       if(result.type === "forum"){
-    //         component = <Forum key={result.id} question={result.question} upvotes={result.upvotes} downvotes={result.downvotes} thread={results.thread} />
-    //       } else if(result.type === "course"){
-    //         component = <Course key={result.id} question={result.question} upvotes={result.upvotes} downvotes={result.downvotes} thread={results.thread} />
-    //       } else if(result.type === "review"){
-    //         component = <Review key={result.id} question={result.question} upvotes={result.upvotes} downvotes={result.downvotes} thread={results.thread} />
-    //       }
-    //       searchLinks += <Route path={"dashboard/result=" + resultID.toString()}>{component}</Route>
-    //   };
+    var searchLinks = this.state.resultsList.map((result) => (
+      <Link
+        key={result.id}
+        to={"dashboard/" + result.type + "=" + result.id.toString()}
+      >
+        {result.type + "-id=" + result.id.toString()}
+      </Link>
+    ));
+    var searchPaths = null;
+    for (var i = 0; i < this.state.resultsList.length; i++) {
+      var result = this.state.resultsList[i];
+      var path = null;
+      if (result.type === "forum") {
+        path = (
+          <Route path={"dashboard/forum=" + result.id.toString()}>
+            <Forums
+              key={result.id}
+              question={result.question}
+              upvotes={result.upvotes}
+              downvotes={result.downvotes}
+              thread={result.thread}
+            />
+          </Route>
+        );
+      } else if (result.type === "course") {
+        path = (
+          <Route path={"dashboard/course=" + result.id.toString()}>
+            <Course
+              key={result.id}
+              question={result.question}
+              upvotes={result.upvotes}
+              downvotes={result.downvotes}
+              thread={result.thread}
+            />
+          </Route>
+        );
+      } else if (result.type === "review") {
+        path = (
+          <Route path={"dashboard/review=" + result.id.toString()}>
+            <Review
+              key={result.id}
+              question={result.question}
+              upvotes={result.upvotes}
+              downvotes={result.downvotes}
+              thread={result.thread}
+            />
+          </Route>
+        );
+      }
+      searchPaths += path;
+    }
     return (
       <div>
         <h1>Search Results</h1>
         {/* {searchResults} */}
         <h1>{this.props.searchKey}</h1>
-        <Router>{}</Router>
+        <Router>
+          {searchLinks}
+          <Switch>{searchPaths}</Switch>
+        </Router>
       </div>
     );
   }
