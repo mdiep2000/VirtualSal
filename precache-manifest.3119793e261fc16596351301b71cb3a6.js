342e58ca48ebcfcd715e6f454a5792f71a5f8903
joshmeidl <josh_meidl@Joshs-MacBook-Pro.local>
2020-04-27 17:14:26 -0700

    Updates

---
+++ precache-manifest.3119793e261fc16596351301b71cb3a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15dd7013bf0420ac5b44015e3b45dbcb",
+    "url": "/VirtualSal/index.html"
+  },
+  {
+    "revision": "034173c668073150b4c4",
+    "url": "/VirtualSal/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0b4a64000d1f6a4fc5ad",
+    "url": "/VirtualSal/static/css/main.86752e3d.chunk.css"
+  },
+  {
+    "revision": "034173c668073150b4c4",
+    "url": "/VirtualSal/static/js/2.50c1564f.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/VirtualSal/static/js/2.50c1564f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4a64000d1f6a4fc5ad",
+    "url": "/VirtualSal/static/js/main.30761b91.chunk.js"
+  },
+  {
+    "revision": "2d45edc884f2c44f87c3",
+    "url": "/VirtualSal/static/js/runtime-main.da7b39ff.js"
+  }
+]);
